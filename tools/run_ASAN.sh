50e637d283d853fe4cbdf167d68b81af13851c51
Aaron Gokaslan <agokaslan@fb.com>
2020-10-29 11:45:59 -0700

    try mosra's suggestion

--- tools/run_ASAN.sh
+++ tools/run_ASAN.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-C_COMPILER_FLAGS="-fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls"
+C_COMPILER_FLAGS="-fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls -pthread"
 export CXXFLAGS="${C_COMPILER_FLAGS}"
 export CFLAGS="${C_COMPILER_FLAGS}"
 GIT_ROOT_DIR="$(git rev-parse --show-toplevel)"
