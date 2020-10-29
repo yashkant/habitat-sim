c9dac575ee98fc8ed8eadef4f54aa3472204577c
Aaron Gokaslan <agokaslan@fb.com>
2020-10-29 12:01:13 -0700

    Try to disable pthreads for GTest

--- tools/run_ASAN.sh
+++ tools/run_ASAN.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-C_COMPILER_FLAGS="-fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls -pthread"
+C_COMPILER_FLAGS="-fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls"
 export CXXFLAGS="${C_COMPILER_FLAGS}"
 export CFLAGS="${C_COMPILER_FLAGS}"
 GIT_ROOT_DIR="$(git rev-parse --show-toplevel)"
@@ -7,5 +7,5 @@ export ASAN_OPTIONS="fast_unwind_on_malloc=0:suppressions=${GIT_ROOT_DIR}/tools/
 export LSAN_OPTIONS="suppressions=${GIT_ROOT_DIR}/tools/lsan_suppressions.txt"
 export CORRADE_TEST_COLOR=ON
 export GTEST_COLOR=yes
-"${GIT_ROOT_DIR}/build.sh" --headless --with-cuda --bullet --run-tests --debug --cmake
+"${GIT_ROOT_DIR}/build.sh" --headless --with-cuda --bullet --run-tests --debug --cmake --cmake-args="-Dgtest_disable_pthreads=ON"
 #TODO Add Python Tests for ASAN
