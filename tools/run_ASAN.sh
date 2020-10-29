d2bc13f2152441e7c03c85b52d13b7943a7b4a04
Aaron Gokaslan <agokaslan@fb.com>
2020-10-29 10:15:19 -0700

    Enable Ubuntu ASAN

--- tools/run_ASAN.sh
+++ tools/run_ASAN.sh
@@ -1,2 +1,11 @@
-#!/bin/bash
-CXXFLAGS="-fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls" CFLAGS="-fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls  " ASAN_OPTIONS="fast_unwind_on_malloc=0:suppressions=$(pwd)/asan_suppressions.txt" LSAN_OPTIONS="suppressions=$(pwd)/lsan_suppressions.txt"  CORRADE_TEST_COLOR=ON GTEST_COLOR=yes ./build.sh --headless --with-cuda --run-tests --debug --bullet
+#!/bin/sh
+C_COMPILER_FLAGS="-fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls"
+export CXXFLAGS="${C_COMPILER_FLAGS}"
+export CFLAGS="${C_COMPILER_FLAGS}"
+GIT_ROOT_DIR="$(git rev-parse --show-toplevel)"
+export ASAN_OPTIONS="fast_unwind_on_malloc=0:suppressions=${GIT_ROOT_DIR}/tools/asan_suppressions.txt"
+export LSAN_OPTIONS="suppressions=${GIT_ROOT_DIR}/tools/lsan_suppressions.txt"
+export CORRADE_TEST_COLOR=ON
+export GTEST_COLOR=yes
+"${GIT_ROOT_DIR}/build.sh" --headless --with-cuda --bullet --run-tests --debug --cmake
+#TODO Add Python Tests for ASAN
