91366b682bfe0b1ed550b3252e6cb0abdd2faa0c
Aaron Gokaslan <agokaslan@fb.com>
2020-10-30 10:07:06 -0700

    Update for Python tests

--- tools/run_ASAN.sh
+++ tools/run_ASAN.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-C_COMPILER_FLAGS="-fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls"
+# export LD_PRELOAD="$(gcc -print-file-name=libasan.so):$LD_PRELOAD"
+export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/nvidia-opengl/:$LD_LIBRARY_PATH
+C_COMPILER_FLAGS="-fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls -g -O1"
 export CXXFLAGS="${C_COMPILER_FLAGS}"
 export CFLAGS="${C_COMPILER_FLAGS}"
 GIT_ROOT_DIR="$(git rev-parse --show-toplevel)"
@@ -7,5 +9,6 @@ export ASAN_OPTIONS="fast_unwind_on_malloc=0:suppressions=${GIT_ROOT_DIR}/tools/
 export LSAN_OPTIONS="suppressions=${GIT_ROOT_DIR}/tools/lsan_suppressions.txt"
 export CORRADE_TEST_COLOR=ON
 export GTEST_COLOR=yes
-"${GIT_ROOT_DIR}/build.sh" --headless --with-cuda --bullet --run-tests --cmake --cmake-args="-Dgtest_disable_pthreads=ON"
+#build/tests/SimTest
+"${GIT_ROOT_DIR}/build.sh" --with-cuda --headless --bullet --build-datatool  --debug --run-tests --cmake --cmake-args="-Dgtest_disable_pthreads=ON"
 #TODO Add Python Tests for ASAN
